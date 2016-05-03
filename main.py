bb4684e24b4cfd1197c015a802abb734d6a6bf1f
carlosTJunior <carlosjunior117@hotmail.com>
2016-05-03 14:22:20 -0300

    32 bits curve in main

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ if __name__ == '__main__':
 #    P = E.point(30, 26)
 #    Q = E.point(35, 41)
 
-    E = EllipticCurve(229, 1, 44)
-    P = E.point(5, 116)
-    Q = E.point(155, 166)
+#    E = EllipticCurve(229, 1, 44)
+#    P = E.point(5, 116)
+#    Q = E.point(155, 166)
 
+    E = EllipticCurve(3753773003, 41779, 81105)
+    P = E.point(1670032921, 3055253653)
+    Q = E.point(1040263251, 2395225033)
     # x = original(E, P, Q)
     # x = serial(E, P, Q)
     x = parallelized(E, P, Q, 4)
--- pollardrho/parallelized.py
+++ pollardrho/parallelized.py
@@ -15,13 +15,14 @@ coefficients = []
 n = 0
 matchPoint = 0
 
-def generate_points(E, P, Q):
+def generate_points(E, P, Q, L):
     global points, lock, match, coefficients, matchPoint, n
     an = gen.randrange(n)
     bn = gen.randrange(n)
     Xn = P*an + Q*bn
+    distinguishedLimit = E.field // L
     while True:
-        if Xn.y < 100:
+        if Xn.y < E.field:
             ct = threading.currentThread().getName()
             #print "{}: x = {}, y = {}\n".format(ct, Xn.x, Xn.y)
             pstr = "({}, {})".format(Xn.x, Xn.y)
@@ -32,11 +33,11 @@ def generate_points(E, P, Q):
             if pstr in points.keys():
                 match = True
                 matchPoint = Xn
-                #print "Thread {} found a match in point {}".format(ct, pstr)
+                print "Thread {} found a match in point {}".format(ct, pstr)
                 coefficients = [an, bn]
                 break
             else:
-                #print threading.currentThread().getName() + ' inserted {}-{}'.format(Xn, [an, bn])
+                print threading.currentThread().getName() + ' inserted {}-{}'.format(Xn, [an, bn])
                 points[pstr] = [an, bn]
                 lock.release()
 
@@ -52,15 +53,18 @@ def parallelized(E, P, Q, numThreads):
 
     global n, matchPoint, points
     n = E.order()
+    print "Order is {}".format(n)
 
     for i in range(L):
+        print "branch created"
         c.append(gen.randrange(n))
         d.append(gen.randrange(n))
         R.append(P*c[-1] + Q*d[-1])
 
     for i in range(numThreads):
+        print "thread created"
         trName = "tr{}".format(i)
-        tr = threading.Thread(target=generate_points, args=(E, P, Q,), name=trName)
+        tr = threading.Thread(target=generate_points, args=(E, P, Q, L,), name=trName)
         tr.start()
         if i == (numThreads - 1):
             tr.join()
